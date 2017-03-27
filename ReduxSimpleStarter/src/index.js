4c0e7bcc6f7f879279280c94500c37774137b83a
Cameron <mooreheadc@hotmail.com>
2017-03-26 21:24:00 -0700

    Embedded video player via youtube API in iframe working

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,21 +1,37 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
-
+import YTSearch from 'youtube-api-search'
 import SearchBar from './components/search_bar'
+import VideoList from './components/video_list'
+import VideoDetail from './components/video_detail'
 
 const API_KEY = 'AIzaSyC58tjw7-BrhVrVrUvCxtxMk5IIk0ZKAlw'
 
-// Create a new component; this component should produce some html
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  )
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      videos: []
+    }
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, videos => {
+      this.setState({ videos })
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.videos[0]} />
+        <VideoList videos={this.state.videos} />
+      </div>
+    )
+  }
 }
 
-// Take this component's generated HTML and put in on the page (in the DOM)
 ReactDOM.render(
   <App />,
   document.querySelector('.container')
