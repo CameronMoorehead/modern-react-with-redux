0f1a23f0a29e57b8679f06b3440c04c68c42b36c
Cameron <mooreheadc@hotmail.com>
2017-03-30 19:16:41 -0700

    Hooked up reducers to view state change

---
+++ tinder-remade/src/containers/book-detail.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+class BookDetail extends Component {
+  render() {
+    return (
+      <div>
+        {this.props.activeBook ? this.props.activeBook.title : 'Select A Book!'} 
+      </div>
+    )
+  }
+
+}
+
+function mapStateToProps(state) {
+  return {
+    activeBook: state.activeBook
+  }
+}
+
+export default connect(mapStateToProps)(BookDetail)
