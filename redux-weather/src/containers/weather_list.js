003371033616920c43ade66b85a9ad13aa69c7c4
Cameron <mooreheadc@hotmail.com>
2017-04-09 01:27:47 -0700

    Added charts

--- redux-weather/src/containers/weather_list.js
+++ redux-weather/src/containers/weather_list.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import WeatherChart from '../components/chart'
 
 class WeatherList extends Component {
   renderWeather(cityData) {
+    const temperatureData = cityData.list.map(temperature => {
+      return temperature.main.temp
+    })
+    const pressureData = cityData.list.map(pressure => {
+      return pressure.main.pressure
+    })
+    const humidityData = cityData.list.map(humidity => {
+      return humidity.main.humidity
+    })
+
     return (
       <tr key={cityData.city.id}>
         <td>{cityData.city.name}</td>
+        <td><WeatherChart color={"red"} data={temperatureData} units={"K"} /></td>
+        <td><WeatherChart color={"blue"} data={pressureData} units={"hPa"}/></td>
+        <td><WeatherChart color={"black"} data={humidityData} units={"%"}/></td>
       </tr>
     )
   }
@@ -16,13 +30,13 @@ class WeatherList extends Component {
         <thead>
           <tr>
             <th>City</th>
-            <th>Temperature</th>
-            <th>Pressure</th>
-            <th>Humidity</th>
+            <th>Temperatur (K)</th>
+            <th>Pressure (HPA)</th>
+            <th>Humidity (pc%)</th>
           </tr>
         </thead>
         <tbody>
-          {this.props.weather.map(this.renderWeather)}
+            {this.props.weather.map(this.renderWeather)}
         </tbody>
       </table>
     )
